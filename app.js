a51f1c575b317161ad3f2c0c1825fae70151c75e
natahwat_thu <n.thumthiwong@gmail.com>
2015-10-12 18:22:35 +0700

    fix test case

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('hello jenkins');
+  res.send('hello jenkins 123');
 });
 app.listen(process.env.PORT || 31332);
 
